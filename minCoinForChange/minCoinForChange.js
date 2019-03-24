028f9f2f9541458b9b42231aeba10405490b367e
seintun <seintun.edu@gmail.com>
2019-03-24 16:35:10 -0700

    Added solution for minCoinForChange

--- minCoinForChange/minCoinForChange.js
+++ minCoinForChange/minCoinForChange.js
@@ -11,4 +11,22 @@ Sample output: 3 (2x1 + 1x5)
 
 function minNumberOfCoinsForChange(n, denoms) {
   // Write your code here.
+  const numOfCoins = (new Array(n + 1).fill(Infinity))
+  numOfCoins[0] = 0;
+  for (const denom of denoms) {
+    for (let amount = 0; amount < numOfCoins.length; amount++) {
+      if (denom <= amount) {
+        numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1)
+      }
+    }
+  }
+  const result = numOfCoins[n] !== Infinity ? numOfCoins[n] : -1
+  console.log(result)
+  return result;
 }
+
+
+
+minNumberOfCoinsForChange(7, [1, 5, 10]) // 3 coins [$1x2, $5x1]
+minNumberOfCoinsForChange(0, [1, 2, 3]) // 0 coin
+minNumberOfCoinsForChange(3, [2, 1]) // 2 coins [$2x1, $1x1]
