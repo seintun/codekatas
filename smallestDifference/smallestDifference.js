ca7b70421f7473432a08b79ce541164e60b76b90
seintun <seintun.edu@gmail.com>
2019-03-24 15:41:14 -0700

    smallestDifference challenge added

---
+++ smallestDifference/smallestDifference.js
@@ -0,0 +1,16 @@
+/**
+ * Write a function that takes in two non-empty integers. The function should find the pair of numbers (one from the first array, one from the secon array) whose absolute difference is closest to zero. The function should return an array containing these two numbers, with the number from the first array in the first position. Assume that there will be only on pair of numbers with the smallest difference.
+ */
+
+// input:[-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]
+// output: [28, 26]
+
+// input:[10, 0 20, 25], [1005, 1006, 1014, 1032, 1031]
+// output: [25, 1005]
+
+const arr1 = [-1, 5, 10, 20, 28, 3]
+const arr2 = [26, 134, 135, 15, 17]
+
+(function smallestDifference(arr1, arr2) {
+
+})(arr1, arr2)
