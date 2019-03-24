4404de28fd5d96b4d9135b8154a09b762eb44cfb
seintun <seintun.edu@gmail.com>
2019-03-24 14:52:21 -0700

    bubble Sort swap es2015 version

---
+++ bubbleSort/bubbleSort2.js
@@ -0,0 +1,28 @@
+const array = [8, 5, 2, 9, 5, 6, 3];
+
+// Unoptimized ES2015 version
+/**
+ * Best: O(n) time O(1) Space
+ * Avg: O(n^2) time O(1) Space
+ * Worst: O(n^2) time O(1) Space
+ */
+(function bubbleSort(array) {
+  const swap = (array, idx1, idx2) => {
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]]
+  }
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1)
+      }
+    }
+  }
+  console.log(array)
+  return array
+})(array) // [ 8, 5, 2, 9, 5, 6, 3 ]
+
+function swap(i, j, array) {
+  const temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
+}
