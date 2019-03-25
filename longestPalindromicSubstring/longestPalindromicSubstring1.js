34f1ef95596f17acbc96a4245ff94e74a32db104
seintun <seintun.edu@gmail.com>
2019-03-24 17:31:11 -0700

    longestPalindromicSubstring1 solution

--- longestPalindromicSubstring/longestPalindromicSubstring1.js
+++ longestPalindromicSubstring/longestPalindromicSubstring1.js
@@ -6,8 +6,30 @@ Sample input:"abaxyzzyxf"
 Sample output:"xyzzyx"
  */
 
+
 function longestPalindromicSubstring(string) {
   // Write your code here.
+  let longest = "";
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      const substring = string.slice(i, j + 1);
+      if (substring.length > longest.length && isPalindrome(substring))
+        longest = substring;
+    }
+  }
+  console.log(longest);
+  return longest;
+}
+
+function isPalindrome(string){
+  let leftIdx = 0,
+    rightIdx = string.length - 1;
+  while (leftIdx < rightIdx) {
+    if (string[leftIdx] !== string[rightIdx]) return false;
+    leftIdx++;
+    rightIdx--;
+  }
+  return true;
 }
 
 longestPalindromicSubstring("abaxyzzyxf") // "xyzzyx"
