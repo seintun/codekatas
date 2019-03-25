63481b19667d72341a8b23c9a99ab17da2826d2d
seintun <seintun.edu@gmail.com>
2019-03-24 17:35:25 -0700

    Added longestPalindromicSubstring2 challenge

---
+++ longestPalindromicSubstring/longestPalindromicSubstring2.js
@@ -0,0 +1,17 @@
+/**
+Longest Palindromic Substring
+Write a function that, given a string, returns its longest palindromic substring. A palindrome is dened as a string that is written the same forward and backward. Assume that there will only be one longest palindromic substring.
+
+Sample input:"abaxyzzyxf"
+Sample output:"xyzzyx"
+ */
+
+//  O(n^3) Time and O(1) space
+function longestPalindromicSubstring(string) {
+  // Write your code here.
+
+}
+
+longestPalindromicSubstring("abaxyzzyxf") // "xyzzyx"
+longestPalindromicSubstring("a") // "a"
+longestPalindromicSubstring("noon high it is") // "noon"
