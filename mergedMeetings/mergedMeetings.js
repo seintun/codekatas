ebc56eb128be9124e8d5b31e7608320b2a3a2cdf
seintun <seintun.edu@gmail.com>
2019-03-26 15:29:27 -0700

    Added solution at O(nlgn) time and O(n)O(n) space

--- mergedMeetings/mergedMeetings.js
+++ mergedMeetings/mergedMeetings.js
@@ -1,4 +1,26 @@
-function mergeRanges(meetings) {}
+function mergeRanges(meetings) {
+  // Merge meetings ranges
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+
+  const sortedMeetings = meetingsCopy.sort((a, b) => a.startTime - b.startTime);
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting = sortedMeetings[i];
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+    if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+      lastMergedMeeting.endTime = Math.max(
+        lastMergedMeeting.endTime,
+        currentMeeting.endTime
+      );
+    } else {
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
+}
 
 // Tests
 
