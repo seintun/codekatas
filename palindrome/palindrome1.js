2d0f38761e9d4081421d55dacb5ff78f902bc398
seintun <seintun.edu@gmail.com>
2019-03-25 21:20:15 -0700

    Palindrome file update

--- palindrome/palindrome1.js
+++ palindrome/palindrome1.js
@@ -1,4 +1,4 @@
-/**
+z/**
 Palindrome Check
 Write a function that takes in a non-empty string and that returns a boolean representing whether or
 not the string is a palindrome. A palindrome is dened as a string that is written the same forward and
