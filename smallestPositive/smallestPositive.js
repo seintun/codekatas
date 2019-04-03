ae4f09ce6eec1a93d6fb162b3967f4031809a9a0
seintun <seintun.edu@gmail.com>
2019-04-03 14:49:04 -0700

    Attempted and added solution for smalledPositive challenge

--- smallestPositive/smallestPositive.js
+++ smallestPositive/smallestPositive.js
@@ -15,3 +15,32 @@ N is an integer within the range [1..100,000];
 each element of array A is an integer within the range [−1,000,000..1,000,000].
  * 
  */
+
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  let m = Math.max(...A);
+  if (m < 1) return 1;
+  if (A.length === 1) return A[0] === 1 ? 2 : 1;
+
+  let i = 0;
+  const l = [m];
+  for (let i = 0; A.length; i++) {
+    if (A[i] > 0) {
+      if (l[A[i] - 1] !== 1) l[A[i] - 1] = 1;
+    }
+  }
+  for (let i = 0; l.length; i++) {
+    if (l[i] === 0) return i + 1;
+  }
+  console.log(i + 2);
+  return i + 2;
+}
+
+const test = [1, 3, 6, 4, 1, 2];
+solution(test);
+
+// Example test:    [1, 3, 6, 4, 1, 2] // expected 5
+
+// Example test:    [1, 2, 3] // expected 4
+
+// Example test:    [-1, -3] // expected 1
