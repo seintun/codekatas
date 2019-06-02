3b34780f2a698c6ed9bb8411efd251948122f803
seintun <seintun.edu@gmail.com>
2019-06-01 23:43:26 -0700

    Added findClosestValueInBst problem with test suite

--- E2-findClosestValueInBst/findClosestValueInBst.js
+++ E2-findClosestValueInBst/findClosestValueInBst.js
@@ -1,3 +1,19 @@
+/**
+ * Find Closest Value in BST
+ *
+ * You are given a BST data  structure consisting of BST ndoes. Each BST node has an integer value stored in a property called "value" and two children nodes stored in properties called "left" and "right" respectively. A node is said to be a BST node if and only if it satisfies the BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves or None (null) values. You are also given a target integer value; write a function that finds the closest value to the target valuue contained in the BST. Assume that there will only be one closes value.
+ *
+ * Sample input:
+ *        10    , 12
+ *       /  \
+ *      5    15
+ *     / \  /  \
+ *    2  5 13  22
+ *   /       \
+ *  1        14
+ * Sample output: 13
+ */
+
 function findClosestValueInBst(tree, target) {
   // Write your code here.
 }
