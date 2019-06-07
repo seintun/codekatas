83e9caec0d9c1011a6f2b4e89478b36ee56f0b15
seintun <seintun.edu@gmail.com>
2019-06-06 17:45:36 -0700

    Added anagramPalindrome problem description

--- E99-anagramPalindrome/anagramPalindrome.js
+++ E99-anagramPalindrome/anagramPalindrome.js
@@ -1,3 +1,14 @@
+/**
+ * anagramPalindrome
+Write a function which accepts an input word and returns true or false if there exists an anagram of that input word that is a palindrome.
+Sample Test 1:
+input: "racecar"
+output: true
+Sample Test 2:
+input: "ab"
+output: false
+ */
+
 function anagramPalindrome(word) {
   // Write your code here.
 }
