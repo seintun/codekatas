bd10783261b483e42f09b52b9efbfbe8a71edcb0
seintun <seintun.edu@gmail.com>
2019-06-06 20:16:20 -0700

    Updated test suites for edge cases for sortedBitSearch

--- E98-sortedBitSearch/test.js
+++ E98-sortedBitSearch/test.js
@@ -1,14 +1,14 @@
 const sortedBitSearch = require("./sortedBitSearch");
 
 it("Test Case #1", function() {
-  const input = [0];
-  const output = -1;
+  const input = [0, 0, 1, 1, 1, 1, 1, 1, 1];
+  const output = 7;
   expect(sortedBitSearch(input)).toEqual(output);
 });
 
 it("Test Case #2", function() {
-  const input = [1];
-  const output = 1;
+  const input = [0, 1, 1, 1, 1];
+  const output = 4;
   expect(sortedBitSearch(input)).toEqual(output);
 });
 
@@ -31,7 +31,13 @@ it("Test Case #5", function() {
 });
 
 it("Test Case #6", function() {
-  const input = [1, 1, 1, 1, 1];
-  const output = 5;
+  const input = [0];
+  const output = 0;
+  expect(sortedBitSearch(input)).toEqual(output);
+});
+
+it("Test Case #7", function() {
+  const input = [1, 1, 1, 1, 1, 1, 1];
+  const output = 7;
   expect(sortedBitSearch(input)).toEqual(output);
 });
