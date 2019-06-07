0124ac4b2ac4e7bd993ce7ad220a600c6bfbf966
seintun <seintun.edu@gmail.com>
2019-06-06 20:22:29 -0700

    Added solution for anagramPalindrome

--- E99-anagramPalindrome/solution/anagramPalindrome.js
+++ E99-anagramPalindrome/solution/anagramPalindrome.js
@@ -1,5 +1,18 @@
 function anagramPalindrome(word) {
   // Write your code here.
+  const letterFreq = {};
+  let hasOdd = 0;
+
+  for (const letter of word) {
+    if (!letterFreq[letter]) letterFreq[letter] = 1;
+    else letterFreq[letter]++;
+  }
+
+  for (const value in letterFreq) {
+    if (letterFreq[value] % 2 === 1) hasOdd++;
+    if (hasOdd > 1) return false;
+  }
+  return true;
 }
 
 module.exports = anagramPalindrome;
