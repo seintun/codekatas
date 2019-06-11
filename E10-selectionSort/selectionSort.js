a4b1474b3ef36486b30b15785b38e03ef9386217
seintun <seintun.edu@gmail.com>
2019-06-10 22:56:52 -0700

    Added selectionSort problem with test suites

---
+++ E10-selectionSort/selectionSort.js
@@ -0,0 +1,10 @@
+// Selection Sort
+// Write a function that takes in an array of integers and returns a sorted version of that array. Use the Selection Sort algorithm to sort the array.
+// Sample input: [8, 5, 2, 9, 5, 6, 3]
+// Sample output: [2, 3, 5, 5, 6, 8, 9]
+
+function selectionSort(array) {
+  // Write your code here.
+}
+
+module.exports = selectionSort;
