ab1fe70a59faa92c42be3c50ef54a17a2709d316
seintun <seintun.edu@gmail.com>
2019-07-19 23:55:43 -0700

    Added minMaxStack problem with test suites

---
+++ M20-minMaxStack/MinMaxStack.js
@@ -0,0 +1,55 @@
+/**
+Min Max Stack Construction
+Write a Min Max Stack class. The class should support pushing and popping values on and off the stack, peeking at values at the top of the stack, and getting both the minimum and the maximum values in the stack. All class methods, when considered independently, should run in constant time and with constant space.
+Sample input:
+-> push(5)
+-> getMin()
+-> getMax()
+-> peek()
+-> push(7)
+-> getMin()
+-> getMax()
+-> peek()
+-> push(2)
+-> getMin()
+-> getMax()
+-> peek()
+-> pop()
+-> pop()
+-> getMin()
+-> getMax()
+-> peek()
+Sample output:
+-> (push 5)
+-> 5 (min)
+-> 5 (max)
+-> 5 (peek)
+-> (push 7)
+-> 5 (min)
+-> 7 (max)
+ */
+
+class MinMaxStack {
+  peek() {
+    // Write your code here.
+  }
+
+  pop() {
+    // Write your code here.
+  }
+
+  push(number) {
+    // Write your code here.
+  }
+
+  getMin() {
+    // Write your code here.
+  }
+
+  getMax() {
+    // Write your code here.
+  }
+}
+
+// Do not edit the line below.
+module.exports = MinMaxStack;
